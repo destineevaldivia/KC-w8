209455eb4b46841e2f408d5103fe13d38d9d80e9
destineevaldivia <destvaldivia@gmail.com>
2023-09-08 16:32:31 -0700

    created test for the header

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,7 @@
 import { render } from '@testing-library/react';
 import App from './App';
 import { expect } from 'vitest';
+import Header from './components/header';
 
 test("say hello to Techtonica", () => {
     // const {debug} = render(<App/>)
@@ -9,3 +10,17 @@ test("say hello to Techtonica", () => {
     expect(getByText("Hello Techtonica!")).toBeDefined();
 
 });
+
+describe("Testing the Header", () => {
+    //Create a test to check that the Header component renders
+    test("It renders the Header", () => {
+    // const {debug} = render(<Header/>)
+    // debug()
+        const { getByTestId } = render(<Header />);
+        expect(getByTestId("header")).toBeDefined();
+    });
+
+    //Create a test to check that the NavBar component renders the name prop
+
+
+})
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-      <div>
+      <div data-testid="header">
         <h1>Hello Techtonica!</h1>
         <h4>This is a Gratitud List</h4>
       </div>
