d908204727c71ec4b64c72bbee2414249c8e6c00
destineevaldivia <destvaldivia@gmail.com>
2023-09-08 17:02:34 -0700

    test to render item components

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,25 +2,46 @@ import { render } from '@testing-library/react';
 import App from './App';
 import { expect } from 'vitest';
 import Header from './components/header';
+import Item from './components/item';
 
-test("say hello to Techtonica", () => {
+//sanity check
+test("say hello to Destinee", () => {
     // const {debug} = render(<App/>)
     // debug()
     const { getByText } = render(<App />);
-    expect(getByText("Hello Techtonica!")).toBeDefined();
+    expect(getByText("Hello Destinee!")).toBeDefined();
 
 });
-
-describe("Testing the Header", () => {
-    //Create a test to check that the Header component renders
-    test("It renders the Header", () => {
+//test #1
+    test("Test to check that the Header component renders", () => {
     // const {debug} = render(<Header/>)
     // debug()
         const { getByTestId } = render(<Header />);
         expect(getByTestId("header")).toBeDefined();
     });
 
-    //Create a test to check that the NavBar component renders the name prop
+//test #2
+    test("Test if items in the list (Item component) are rendered correctly", () => {
+        const items = [
+            { text: "dog" },
+            { text: "family" },
+            { text: "friends" },
+        ];  
+        const { getByText } = render(
+            <div>
+                {items.map((item, index) => (
+                    <Item key={index} item={item} />
+                ))}
+            </div>
+        );
+        items.forEach((item) => {
+            expect(getByText(item.text)).toBeDefined();
+        });
+    })
+//test #3
+//Test if the Form component works as expected or is rendered correctly.
+
+//test#4
+//Test any other functionality or edge case you think is necessary.
 
 
-})
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
       <div data-testid="header">
-        <h1>Hello Techtonica!</h1>
+        <h1>Hello Destinee!</h1>
         <h4>This is a Gratitud List</h4>
       </div>
     );
